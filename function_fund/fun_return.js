d25a2a9ec68768073ecb5325996aa987819a1f3a
RoufRain <roufrain91@gmail.com>
2025-02-15 11:33:02 +0600

    condtional return added

--- function_fund/fun_return.js
+++ function_fund/fun_return.js
@@ -1,8 +1,19 @@
 console.log("Return============return");
 
-function tenTimes(num) {
-  const res = num * 10;
-  return res;
+// function tenTimes(num) {
+//   const res = num * 10;
+//   return res;
+// }
+// const output = tenTimes(258);
+// console.log(output);
+
+//conditional return
+function isOdd(number) {
+  if (number % 2 === 1) {
+    return true;
+  } else {
+    return false;
+  }
 }
-const output = tenTimes(258);
-console.log(output);
+const result = isOdd(7);
+console.log(result);
