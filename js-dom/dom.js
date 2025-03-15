1e27dd3f8bd5334a5222062e9c28acadaed00582
RoufRain <roufrain91@gmail.com>
2025-03-15 19:48:36 +0600

    querySelector ,querySelectorAll  add

--- js-dom/dom.js
+++ js-dom/dom.js
@@ -2,7 +2,7 @@ console.log("hello , this is js dom file");
 console.log(document.body);
 const innerLi = document.getElementsByTagName("li");
 for (li of innerLi) {
-  console.log(li.innerText);
+  //   console.log(li.innerText);
 }
 
 const place = document.getElementById("place-title");
@@ -12,4 +12,13 @@ const placeName = document.getElementsByClassName("place-name");
 
 for (let place of placeName) {
   console.log(place.innerText);
+}
+// queryselector,queryselectorall for css
+
+let someLi = document.querySelector(".fruits-container li");
+console.log(li);
+
+const fruitsName = document.querySelectorAll(".fruits-container li");
+for (let fruit of fruitsName) {
+  console.log(fruit.innerText);
 }
