d6f5616a494852dbaaee6c8007a67605b7c0d533
RoufRain <roufrain91@gmail.com>
2025-03-15 11:04:29 +0600

    get elemetById and getelementbyclass,getElementbyTagname add

--- js-dom/dom.js
+++ js-dom/dom.js
@@ -4,3 +4,12 @@ const innerLi = document.getElementsByTagName("li");
 for (li of innerLi) {
   console.log(li.innerText);
 }
+
+const place = document.getElementById("place-title");
+console.log(place);
+
+const placeName = document.getElementsByClassName("place-name");
+
+for (let place of placeName) {
+  console.log(place.innerText);
+}
