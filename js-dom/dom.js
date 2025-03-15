a3aef68ea0dcf268bdffc2b89195f98cc2a1e833
RoufRain <roufrain91@gmail.com>
2025-03-15 23:48:59 +0600

    html style using js dom,file domstylewithjs  add

--- js-dom/dom.js
+++ js-dom/dom.js
@@ -1,24 +1,24 @@
 console.log("hello , this is js dom file");
-console.log(document.body);
+// console.log(document.body);
 const innerLi = document.getElementsByTagName("li");
 for (li of innerLi) {
   //   console.log(li.innerText);
 }
 
 const place = document.getElementById("place-title");
-console.log(place);
+// console.log(place);
 
 const placeName = document.getElementsByClassName("place-name");
 
 for (let place of placeName) {
-  console.log(place.innerText);
+  //   console.log(place.innerText);
 }
 // queryselector,queryselectorall for css
 
 let someLi = document.querySelector(".fruits-container li");
-console.log(li);
+// console.log(li);
 
 const fruitsName = document.querySelectorAll(".fruits-container li");
 for (let fruit of fruitsName) {
-  console.log(fruit.innerText);
+  //   console.log(fruit.innerText);
 }
---
+++ js-dom/domstylewithjs.js
@@ -0,0 +1,8 @@
+const sections = document.querySelectorAll("section");
+for (section of sections) {
+  section.style.border = "3px solid blue";
+  section.style.borderRadius = "15px";
+  section.style.marginBottom = "5px";
+}
+const sectionStyle = document.getElementById("places-container");
+sectionStyle.style.backgroundColor = "yellow";
