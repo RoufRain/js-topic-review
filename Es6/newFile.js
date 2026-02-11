640280f67e3c5fe73604f93a7862b239dcaf5703
RoufRain <roufrain91@gmail.com>
2026-02-11 20:53:24 +0600

    array loop

---
+++ Es6/newFile.js
@@ -0,0 +1,14 @@
+// let salaries = {
+//   rain: 344,
+//   rahm: 343,
+//   mujahid: 34,
+// };
+// function topSalaries(salaries) {
+//     
+// }
+
+let numbers=[1,4,5,6,3,2,4];
+for(i=0;i<numbers.length;i++){
+  let l1=numbers[i]+1;
+  console.log(l1);
+}
